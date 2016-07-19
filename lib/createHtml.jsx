10f1d006f98664aa91131b6ffa3d82c5c61c2907
Sam Jeffress <s.jeffress@nib.com.au>
2016-07-19 13:57:23 +1000

    adding return of vwo2()

--- lib/createHtml.jsx
+++ lib/createHtml.jsx
@@ -19,7 +19,7 @@ function vwo1(vwoAccountId){
 }
 
 function vwo2(){
-   `
+   return `
   if(typeof(_vis_opt_settings_loaded) == "boolean") { document.write('<s' + 'cript src="' + _vis_opt_protocol +
   'd5phz18u4wuww.cloudfront.net/vis_opt.js" type="text/javascript">' + '<\/s' + 'cript>'); }
   // if your site already has jQuery 1.4.2, replace vis_opt.js with vis_opt_no_jquery.js above
